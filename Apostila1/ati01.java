a4b3882be1144a99347bcf4884b00d637ce6be03
Djenifer Almeida <117329548+Djenifer-Almeida@users.noreply.github.com>
2023-07-06 18:52:35 -0300

    Update ati01.java

--- Apostila1/ati01.java
+++ Apostila1/ati01.java
@@ -1,6 +1,5 @@
-public class ati01 {
-    public static void main(String[] args) {
-        System.out.println("Meu nome é Djenifer");
-
+    public class ati01{
+    public static void main(String[] args){
+    System.out.println("Meu nome é Djenifer");
     }
-}
+    }
