442747005f89e53f0bafaaa6b21a89cd263a6062
Djenifer Almeida <117329548+Djenifer-Almeida@users.noreply.github.com>
2023-07-06 21:45:55 -0300

    Update ati07.java

--- Apostila1/ati07.java
+++ Apostila1/ati07.java
@@ -1,4 +1,4 @@
-        public class Exercicio7 {
+        public class Ati07{
         public static void main(String[] args) {
         System.out.println("           $           ");
         System.out.println("        $      $       ");
