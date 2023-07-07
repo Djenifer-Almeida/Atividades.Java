639dcbac21b03af22eccaa026c7570f5ef9a5231
Djenifer Almeida <117329548+Djenifer-Almeida@users.noreply.github.com>
2023-07-06 22:35:28 -0300

    Update ati06.java

--- Apostila1/ati06.java
+++ Apostila1/ati06.java
@@ -1,3 +1,4 @@
+        //6-Desenhe um losango utilizando o caractere ponto (.) com largura de 7 caracteres.
         public class ati06 {
         public static void main(String[] args) {
         System.out.println("           .           ");
