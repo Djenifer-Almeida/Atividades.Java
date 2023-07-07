3376542dba49281e3e3da2701a1ea3fc5897db05
Djenifer Almeida <117329548+Djenifer-Almeida@users.noreply.github.com>
2023-07-06 22:32:42 -0300

    Update ati02.java

--- Apostila1/ati02.java
+++ Apostila1/ati02.java
@@ -1,3 +1,4 @@
+        //2-Escreva um programa que imprima a mensagem "A soma de 2 + 2 é igual a 4".
         public class ati02 {
         public static void main(String[] args){
         System.out.println("A soma de 2 + 2 é 4");
