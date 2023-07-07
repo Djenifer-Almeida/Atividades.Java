0f523a253a7ebef1eabdd76d68287bb855356173
Djenifer Almeida <117329548+Djenifer-Almeida@users.noreply.github.com>
2023-07-06 22:36:32 -0300

    Update Exercicio08 (1).java

--- Apostila1/Exercicio08 (1).java
+++ Apostila1/Exercicio08 (1).java
@@ -1,3 +1,4 @@
+        //8-Desenhe uma árvore de Natal utilizando o caractere asterisco (*) com altura de 8 linhas
         public class Exercicio08 {
         public static void main(String[] args) {
         System.out.println("      * _-**         ");
