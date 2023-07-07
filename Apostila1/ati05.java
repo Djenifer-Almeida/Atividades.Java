6af4039a54001f84bc236413ab11ebebdaae3c9d
Djenifer Almeida <117329548+Djenifer-Almeida@users.noreply.github.com>
2023-07-06 22:34:40 -0300

    Update ati05.java

--- Apostila1/ati05.java
+++ Apostila1/ati05.java
@@ -1,3 +1,4 @@
+        //5-Desenhe um triângulo utilizando o caractere hashtag (#) com altura de 6 linhas.
         public class ati05 {
         public static void main(String[] args){
         System.out.println("                #                ");
