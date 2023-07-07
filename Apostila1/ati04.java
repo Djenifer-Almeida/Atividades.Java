c6928393430b7c08669e1f7a8efabdd4191064fc
Djenifer Almeida <117329548+Djenifer-Almeida@users.noreply.github.com>
2023-07-06 22:34:02 -0300

    Update ati04.java

--- Apostila1/ati04.java
+++ Apostila1/ati04.java
@@ -1,3 +1,4 @@
+        //4-Desenhe um quadrado vazado utilizando o caractere asterisco (*) com lado de tamanho 5.
         public class ati04 {
         public static void main(String[] args){
         System.out.println("*   *    *    *   *    * ");
