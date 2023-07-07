2cc364e32eeaf4eeaa2e44df74c43f196fb12086
Djenifer Almeida <117329548+Djenifer-Almeida@users.noreply.github.com>
2023-07-06 22:33:25 -0300

    Update ati03.java

--- Apostila1/ati03.java
+++ Apostila1/ati03.java
@@ -1,3 +1,4 @@
+        //3-Escreva um programa que imprima uma linha com dez asteriscos (*) consecutivos.
         public class ati03{
         public static void main(String[] args) {
         System.out.println("***********");
