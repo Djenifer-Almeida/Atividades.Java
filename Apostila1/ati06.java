7ce839a1017fa1b91cc8f328a36d2919510b8651
Djenifer Almeida <117329548+Djenifer-Almeida@users.noreply.github.com>
2023-07-06 21:53:12 -0300

    Update ati06.java

--- Apostila1/ati06.java
+++ Apostila1/ati06.java
@@ -1,4 +1,4 @@
-        public class Exercicio6 {
+        public class ati06 {
         public static void main(String[] args) {
         System.out.println("           .           ");
         System.out.println("        .     .        ");
