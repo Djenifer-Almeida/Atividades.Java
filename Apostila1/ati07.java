91087bd127ff4f40dfb3f7203803a50dc081f791
Djenifer Almeida <117329548+Djenifer-Almeida@users.noreply.github.com>
2023-07-06 22:36:01 -0300

    Update ati07.java

--- Apostila1/ati07.java
+++ Apostila1/ati07.java
@@ -1,3 +1,4 @@
+        //7-Desenhe uma pirâmide utilizando o caractere cifrão ($) com altura de 5 linhas
         public class ati07{
         public static void main(String[] args) {
         System.out.println("           $           ");
